d9eb350ea0edb1404febd8ef7e6181fc21348c25
bcorwin <ben.corwin@raise.com>
2018-09-26 17:09:00 -0500

    added with_moon lamp ability

--- lamp_updater.py
+++ lamp_updater.py
@@ -11,6 +11,7 @@ import csv
 import datetime
 
 MOON_INFO_FILE = config.FILE_PATHS['moon_info']
+MOON_TIMES_FILE = config.FILE_PATHS['moon_times_file']
 
 with open(config.FILE_PATHS['moon_lamp'], 'r') as f:
   data = json.load(f)
@@ -26,6 +27,22 @@ with open(MOON_INFO_FILE, 'r') as f:
       CURRENT_INFO = row
       break
 
+CURRENT_PHEN = None
+current_dt = datetime.datetime.utcnow()
+with open(MOON_TIMES_FILE, 'r') as f:
+  reader = csv.reader(f)
+  prev_phen, prev_time = None, None
+  for row in reader:
+    phen, time = row
+    time = datetime.datetime.strptime(time, "%Y-%m-%d %H:%M:%S")
+    
+    if prev_time is not None and current_dt > prev_time and current_dt <= time:
+      CURRENT_PHEN = prev_phen
+      break
+    
+    prev_phen, prev_time = phen, time
+
+    
 def get_current_phase():
   """
   Reads the moon lamp config file and returns the current phase number
@@ -50,7 +67,7 @@ elif phase_mode == 'hold':
   phase_num = config.PHASE_NAMES[phase_phase]
   pin_status = config.LAMP_PHASES[phase_num]
 else:
-  # ERROR, phase_mode not defined
+  # ERROR: phase_mode not defined
   pin_status = ['on', 'off', 'on', 'off', 'on', 'off']
 
 # Update lamp status in lights.json
@@ -58,8 +75,13 @@ if lamp_mode == 'on':
   light_on = True
 elif lamp_mode == 'off':
   light_on = False
+elif lamp_mode == 'with_moon':
+  light_on = False if CURRENT_PHEN == 'S' else True
+  if CURRENT_PHEN not in ['S', 'R']:
+    # ERROR: moon_times not available
+    pin_status = ['off', 'off', 'on', 'on', 'off', 'off']  
 else:
-  # ERROR, lamp_mode not defined
+  # ERROR: lamp_mode not defined
   pin_status = ['off', 'on', 'off', 'on', 'off', 'on'] 
   light_on = True
   
