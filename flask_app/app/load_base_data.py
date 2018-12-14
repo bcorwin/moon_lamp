c00790c907d88e4ae6049115b413d8e2438f81f7
bcorwin <ben.corwin@raise.com>
2018-12-14 10:03:18 -0600

    added moon_rise_time and reset db

--- flask_app/app/load_base_data.py
+++ flask_app/app/load_base_data.py
@@ -1,28 +1,34 @@
 from app import db
 from app.models import ErrorCode
 
-# DO NOT CHANGE THE ORDER OF THE BELOW ERRORS
 error_codes = [
   {
+    'error_code': 0,
     'description': 'phase_mode not defined',
     'light_pattern': 42
   },
   {
+    'error_code': 1,
     'description': 'moon_times not available',
     'light_pattern': 12
   },
   {
+    'error_code': 2,
     'description': 'lamp_mode not defined',
     'light_pattern': 21
   },
   {
+    'error_code': 3,
     'description': 'Invalid phase number. Is moon info updated?',
     'light_pattern': 30
   },
 ]
 
 for i in error_codes:
-    e = ErrorCode(description=i["description"], light_pattern=i["light_pattern"])
+    e = ErrorCode(
+        error_code=i["error_code"],
+        description=i["description"],
+        light_pattern=i["light_pattern"])
     db.session.add(e)
 
 db.session.commit()
--- flask_app/app/models.py
+++ flask_app/app/models.py
@@ -1,18 +1,29 @@
 from app import db
+from datetime import datetime
 
 class ErrorCode(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String(100))
-    light_pattern = db.Column(db.Integer, unique=True)
+    error_code = db.Column(db.Integer, unique=True, nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+    light_pattern = db.Column(db.Integer, unique=True, nullable=False)
 
     def __repr__(self):
         return(f"<Error: {self.description} (ErrorCode={self.id})>")
 
 class MoonPhase(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(16))
-    display_name = db.Column(db.String(16))
-    light_pattern = db.Column(db.Integer, unique=True)
+    name = db.Column(db.String(16), nullable=False)
+    display_name = db.Column(db.String(16), nullable=False)
+    light_pattern = db.Column(db.Integer, unique=True, nullable=False)
 
     def __repr__(self):
         return(f"<MoonPhase: {display_name} ({light_pattern})")
+
+class MoonRiseTime(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    start_at = db.Column(db.DateTime, unique=True, nullable=False)
+    moon_phase_id = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return(f"<MoonRiseTime: {moon_phase_id} starts at {start_at}>")
