d2e0c040e0255b909fb584ae715892c4765ca273
Ben Corwin <ben@threadtogether.com>
2020-12-06 14:43:11 -0600

    make work on mac

--- set_lamp.sh
+++ set_lamp.sh
@@ -2,13 +2,13 @@
 
 # Kill lamp procces, moonlamp, and its child
 CPID=$(pgrep -P $(cat moonlamp.pid))
-sudo kill -9 $(cat setlamp.pid)
-sudo kill -9 $(cat moonlamp.pid)
-sudo kill -9 $CPID
+sudo kill -s TERM $(cat setlamp.pid)
+sudo kill -s TERM $(cat moonlamp.pid)
+sudo kill -s TERM $CPID
 
 # Make sure the process was actually killed
 sleep .5
-PID=$(<"/home/pi/moon_lamp/moonlamp.pid")
+PID=$(<"moonlamp.pid")
 if [ -e /proc/$PID ]
 then
     echo "Process: $PID is still running"
@@ -17,13 +17,13 @@ fi
 
 # Start the new prcoess
 echo $$ > setlamp.pid
-sudo  python3 set_lamp.py "$@" & echo $! > /home/pi/moon_lamp/moonlamp.pid
+sudo  python3 set_lamp.py "$@" & echo $! > moonlamp.pid
 
 # If above fails, set lamp to a failed pattern
-if  wait $(<"/home/pi/moon_lamp/moonlamp.pid")
+if  wait $(<"moonlamp.pid")
 then
     true
 else
-    sudo python3 set_lamp.py --phase-mode=fixed --phase-number=-1 & echo $! >/home/pi/moon_lamp/moonlamp.pid
+    sudo python3 set_lamp.py --phase-mode=fixed --phase-number=-1 & echo $! >moonlamp.pid
     exit 1
 fi
