754dbc11d5398c7a2001aedb3afc3a29365690de
Ben Corwin <ben@threadtogether.com>
2020-12-08 13:18:19 -0600

    add with moon lamp mode

--- set_lamp.py
+++ set_lamp.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
-
+import os
 import click
 import moon_lamp as ml
 from time import sleep
+from requests import get
+from dotenv import load_dotenv
 from datetime import datetime as dt
 from datetime import timedelta, time, datetime
+
+load_dotenv(override=True)
 
 CURRENT_PHASE = -1
 ON_HOUR = 8
@@ -33,6 +37,39 @@ def get_switch_datetime(hour, tomorrow=False):
         current_dt = current_dt + timedelta(hours=24)
     out = datetime(current_dt.year, current_dt.month, day=current_dt.day, hour=hour)
     return out
+
+
+def get_moon_times(current_dt=dt.now()):
+    results = []
+    for delta in [-24, 0, 24, 48]:
+        date = current_dt + timedelta(hours=delta)
+        res = get("https://api.ipgeolocation.io/astronomy",
+                  params={"apiKey": os.getenv("API_KEY"), "date": date.strftime("%Y-%m-%d")})
+        res.raise_for_status()
+        data = res.json()
+
+        for s in ["rise", "set"]:
+            value = data.get(f"moon{s}")
+            value = time(int(value[:2]), int(value[3:])) if value != '-:-' else None
+            if value:
+                value = datetime(year=date.year, month=date.month, day=date.day, hour=value.hour, minute=value.minute)
+                results += [(s, value)]
+    results = sorted(results, key = lambda x: x[1])
+
+    for i in range(len(results)):
+        if results[i][1] < dt.now() <= results[i + 1][1]:
+            if results[i][0] == "rise":
+                moon_up = True
+                moon_rise = results[i][1]
+                moon_set = results[i+1][1]
+            else:
+                # Return next moon rise/set
+                moon_up = False
+                moon_rise = results[i+1][1]
+                moon_set = results[i+2][1]
+            break
+
+    return moon_rise, moon_set, moon_up
 
 
 @click.command()
@@ -69,10 +106,13 @@ def set_lamp(phase_mode, phase_number, phase_length, lamp_mode, timer_length):
             lamp_on = False
             on_at = get_switch_datetime(ON_HOUR, True)  # 8am tomorrow
             off_at = get_switch_datetime(OFF_HOUR, True)  # 8pm tomorrow
-        print(f"Initial on at: {on_at}")
-        print(f"Initial off at: {off_at}")
+    elif lamp_mode == "with_moon":
+        on_at, off_at, lamp_on = get_moon_times()
     else:
         raise ValueError(f"Invalid value for lamp_mode: {lamp_mode}")
+
+    print(f"Initial on at: {on_at}")
+    print(f"Initial off at: {off_at}")
 
     if not lamp_on:
         ml.set_lights(0)
@@ -88,6 +128,10 @@ def set_lamp(phase_mode, phase_number, phase_length, lamp_mode, timer_length):
             elif lamp_on and lamp_mode == "day_only":
                 on_at = get_switch_datetime(ON_HOUR, True)
                 off_at = get_switch_datetime(OFF_HOUR, True)
+                print(f"Next on at: {on_at}")
+                print(f"Next off at: {off_at}")
+            elif lamp_on and lamp_mode == "with_moon":
+                on_at, off_at, _ = get_moon_times()
                 print(f"Next on at: {on_at}")
                 print(f"Next off at: {off_at}")
             lamp_on = False
