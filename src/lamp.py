61bfa37ddac1dcf08c46beac8a7499155e8e1757
Ben Corwin <ben@threadtogether.com>
2020-12-26 10:36:51 -0600

    swap fore and back colors, fixed color order

--- src/lamp.py
+++ src/lamp.py
@@ -21,8 +21,10 @@ class Lamp:
 
             self.pixels = neopixel.NeoPixel(pixel_pin, num_leds, brightness=1,
                                             auto_write=False, pixel_order=pixel_order)
+            self.pixel_order = pixel_order
         else:
             self.pixels = None
+            self.pixel_order = "RGB"
 
         self.reverse_leds = reverse_leds
         self.print_only = print_only
@@ -36,7 +38,7 @@ class Lamp:
 
         hex_colors = ['#%02x%02x%02x' % c for c in colors]
         print_string = [str(datetime.now())]
-        print_string.extend([colr.color(c, fore=c, back=contrast_color(c)) for c in hex_colors])
+        print_string.extend([colr.color(c, fore=contrast_color(c), back=c) for c in hex_colors])
         print_string = "\t".join(print_string)
         print(print_string)
         with open("./lamp.txt", "w") as f:
@@ -48,6 +50,8 @@ class Lamp:
         if not self.print_only:
             for i in range(len(colors)):
                 color = colors[i]
+                if self.pixel_order == "GRB":
+                    color = (color[1], color[0], color[2])
                 led_idx = i if not self.reverse_leds else self.num_leds - i - 1
                 self.pixels[led_idx] = color
             self.pixels.show()
